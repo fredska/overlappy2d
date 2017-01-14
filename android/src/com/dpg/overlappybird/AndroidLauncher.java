8c51ea15ae1b3f184b9ddb806bda49087eae20e8
Fred <darkpoolgames@gmail.com>
2017-01-14 01:36:08 -0500

    Initial commit

---
+++ android/src/com/dpg/overlappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dpg.overlappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dpg.overlappybird.OverlappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new OverlappyBird(), config);
+	}
+}
---
+++ desktop/src/com/dpg/overlappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dpg.overlappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dpg.overlappybird.OverlappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OverlappyBird(), config);
+	}
+}
